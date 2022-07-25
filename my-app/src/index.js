50ce8648aa8670b5269afa9763925e302e2be82a
sugyeong-lee <waterscape2021@naver.com>
2022-07-26 02:19:01 +0900

    시계 렌더 부분 다시

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import App from './App';
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>
-);
+, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
