f1a310906a5fab2b5989961590819bff55728b98
sugyeong-lee <waterscape2021@naver.com>
2022-08-03 23:24:06 +0900

    22/08/03

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,12 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clock />
+    <CommentList />
   </React.StrictMode>
 );
 
