eec993a44f848d0e49c1dc963323d04ad25afd21
sugyeong-lee <waterscape2021@naver.com>
2022-08-06 02:26:31 +0900

    22/08/06

--- my-app/src/chapter_05/Comment.jsx
+++ my-app/src/chapter_05/Comment.jsx
@@ -35,12 +35,19 @@ const styles = {
 function Comment(props) {
     return (
         <div style={styles.wrapper}>
-            
-        </div>
-    <div>
-        <h1>제가 만든 첫 컴포넌트입니다.</h1>
-    </div>
-    );
+            <div style={styles.imageContainer}>
+                <img
+                src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+                style={styles.image}
+                />
+            </div>
 
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>{props.name}</span>
+                <span style={styles.commentText}>{props.comment}</span>
+            </div>
+        </div>
+    );
+}
 
 export default Comment;
--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = {
+    {
+        name:"이인제",
+        comment: "안녕하세요, 소플입니다.",
+    },
+    {
+        name:"이인수",
+        comment: "안녕하세요, 소플입니다.",
+    },
+    {
+        name:"이인자",
+        comment: "안녕하세요, 소플입니다.",
+    },
+};
+
+
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={comment.name} comment={comment.comment} />
         </div>
     );
 }
